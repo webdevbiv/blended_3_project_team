31e262e0fbd923cfdba82d3f563a80f14539c957
Alexandr-Yermolov <dirynchik@gmail.com>
2021-06-12 12:42:37 +0300

    добавил модальное окно на ебаут

---
+++ src/js/mobile-menus.js
@@ -0,0 +1,13 @@
+ (() => {
+const menuBtnRef = document.querySelector("[data-menu-buttons]");
+const mobileMenuRef = document.querySelector("[data-menus]");
+const mobileBtnClose = document.querySelector("[data-menu-closes]");
+            
+menuBtnRef.addEventListener("click", () => {
+mobileMenuRef.classList.toggle("is-opens");
+});
+            
+mobileBtnClose.addEventListener("click", () => {
+mobileMenuRef.classList.toggle("is-opens");
+});
+})();
